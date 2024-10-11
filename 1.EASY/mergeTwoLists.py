36805734adda4adbb68c06783bf9f5336d3e8ff9
Arian Farnoosh <ariyanfar81@gmail.com>
2024-10-11 19:57:54 +0330

    Add files via upload

---
+++ 1.EASY/mergeTwoLists.py
@@ -0,0 +1,24 @@
+class Solution(object):
+    def mergeTwoLists(self, l1, l2):
+        """
+        :type list1: Optional[ListNode]
+        :type list2: Optional[ListNode]
+        :rtype: Optional[ListNode]
+        """
+        dummy = ListNode()
+        tail = dummy
+
+        while l1 and l2:
+            if l1.val < l2.val:
+                tail.next = l1
+                l1 = l1.next
+            else:
+                tail.next = l2
+                l2 = l2.next
+            tail = tail.next
+        if l1:
+            tail.next = l1
+        elif l2:
+            tail.next = l2
+
+        return dummy.next  # Return dummy.next instead of dummy
